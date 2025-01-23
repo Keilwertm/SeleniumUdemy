ec376520779e4769ece6a7bf119ca9c8e3072455
Mel Keilwert <keilwertmelg@gmail.com>
2025-01-23 15:05:18 -0700

    Working on unit tests some more

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ namespace UnitTestProject1;
 public class Tests
 {
     [SetUp]
+    
     public void Setup()
     {
     }
@@ -10,6 +11,26 @@ public class Tests
     [Test]
     public void Test1()
     {
+        var a = 1;
+        var b = 2;
+        Assert.AreEqual(3, a + b);
+        var c = 1;
+        Assert.IsTrue(c == 1);
         Assert.Pass();
+        var d = 1;
+        Assert.AreNotEqual(1, a);
+        
+       
+    }
+
+    private void Quiz()
+    {
+        var a = 3;
+        var b = 2;
+        var c = 1;
+        Assert.AreEqual(5, a + b);
+        Assert.AreEqual(2, c + c);
+        Assert.AreNotEqual(5, a);
+        Assert.Fail("this should fail");
     }
 }
