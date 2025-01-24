3e0ceab992c23b5f2734c9116af8d78744c653c5
Mel Keilwert <keilwertmelg@gmail.com>
2025-01-24 14:50:03 -0700

    Finishing Temperature puzzle

--- Section5/Section5/SectionQuiz.cs
+++ Section5/Section5/SectionQuiz.cs
@@ -6,42 +6,41 @@ namespace Section5
     [TestClass]
     public class SectionQuiz
     {
+        // F to C subtract by 30 then divide by 2
+        
         [TestMethod]
         public void Convert_F_To_C()
         {
-            //given the temperature in fahrenheit use a math
-            //expression to convert the temperature to celsius
-            //display a output string to the console that says
+            Console.WriteLine("What temperature is it outside in Fahrenheit?");
+            string input = Console.ReadLine(); 
+            int inputInt = Convert.ToInt32(input);
+            double celsius = (inputInt - 30) / 2;
+            Console.WriteLine($"The temp in F {input} is {celsius} C");
+
+            //given the temperature in Fahrenheit use a math
+            //expression to convert the temperature to Celsius
+            //display an output string to the console that says
             //The temp in F {given temp} is {temp} in C
             //replacing the values as necessary in the string
 
-            double tempInF = 57;
-            //To convert temperatures in degrees Fahrenheit to Celsius, subtract 32 and multiply by .5556 (or 5/9)
-            double tempInC = (tempInF - 32) * .5556;
-            Console.WriteLine($"The temp in F {tempInF} is {tempInC} in C");
-
-            //check the work
-            //57F should be 13.89C
-            Assert.AreEqual(tempInC, 13.89, 0.001);
         }
 
         [TestMethod]
         public void Convert_C_To_F()
         {
-            //given the temperature in celsius use a math
-            //expression to convert the temperature to fahrenheit
-            //display a output string to the console that says
+            
+            Console.WriteLine("What temperature is it outside in Celcius?");
+            string input = Console.ReadLine(); 
+            int inputInt = Convert.ToInt32(input);
+            double fahrenheit = (inputInt / 5) + 32;
+            Console.WriteLine($"The temp in C {input} is {fahrenheit} F");
+
+            //given the temperature in Celsius use a math
+            //expression to convert the temperature to Fahrenheit
+            //display an output string to the console that says
             //The temp in C {given temp} is {temp} in F
             //replacing the values as necessary in the string
-
-            double tempInC = 12.5;
-            //To convert temperatures in degrees Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32.
-            double tempInF = (tempInC * 1.8) + 32;
-            Console.WriteLine($"The temp in C {tempInC} is {tempInF} in F");
-
-            //check the work
-            //12.5C to F should be 54.5
-            Assert.AreEqual(tempInF, 54.5);
+         
         }
     }
 }
